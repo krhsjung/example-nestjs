ee8778dab7ac40c3223d28cf797367efc812104a
krhsjung <kr.hs.jung@gmail.com>
2024-08-23 18:03:28 +0900

    [ADD] Monorepo structure

--- apps/api/src/api.service.ts
+++ apps/api/src/api.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AppService {
+export class ApiService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello api!';
   }
 }
